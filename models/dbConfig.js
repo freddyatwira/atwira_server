8966c37a46c0cbb2d63082f6e98bf7df794f04b8
freddywaigwa <freddywaigwa@gmail.com>
2023-06-02 14:46:37 +0300

    model changed

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { DB_URL } from "../config.js";
 
-export const db = mongoose.connect(DB_URL, {
+export const db = mongoose.connect(`${DB_URL}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

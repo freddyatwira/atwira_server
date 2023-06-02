4e479081530444ee45d32e3429b665e46022bb69
freddywaigwa <freddywaigwa@gmail.com>
2023-06-02 16:46:29 +0300

    added login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import { User } from "../models/UserModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -29,6 +30,38 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //check if user email is availbale
+
+    const user = await User.findOne({ email });
+
+    if (!user)
+      return res
+        .status(400)
+        .json({ message: "No user with associated with that email" });
+
+    //compare password
+    const isPassword = await bcrypt.compare(password, user.password);
+
+    if (!isPassword)
+      return res.status(400).json({ message: "Password do not match" });
+
+    //everything is right, ingest token
+
+    const token = jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN, {
+      expiresIn: "100s",
+    });
+    res.cookie("accessToken", token, { httpOnly: true });
+    return res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const logout = (req, res) => {};
